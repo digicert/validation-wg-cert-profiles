5f63038a1358df4e6ba21d1755c90443b1bea957
Corey Bonnell <Corey.Bonnell@digicert.com>
2020-11-11 17:12:52 -0500

    Fix regex to not recursively match existing links

--- scripts/linkerizer.rb
+++ scripts/linkerizer.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 def filter_for_rfc(content)
-    r = /RFC+\s(?<rfcnum>\d+),\s+(?<secprefix>(Appendix|Section))\s+(?<secnum>((\d|\w)+\.?){1,4})/
+    r = /(?<!\[)RFC\s+(?<rfcnum>\d+),\s+(?<secprefix>(Appendix|Section))\s+(?<secnum>((\d|\w)+\.?){1,4})(?!\])/
     content.gsub(r) {|m| "[#{$&}](https://tools.ietf.org/html/rfc#{$1}##{$2.downcase}-#{$3})"}
 end
 
